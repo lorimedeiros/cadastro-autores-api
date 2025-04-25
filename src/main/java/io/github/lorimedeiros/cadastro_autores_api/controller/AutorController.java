f62e27dd92a1bcc840aad027def872c170ef3034
Lori Medeiros <cecilialmedeirosc@gmail.com>
2025-04-25 20:04:23 -0300

    1.7 Pesquisa (GET) de Autores com filtro

--- src/main/java/io/github/lorimedeiros/cadastro_autores_api/controller/AutorController.java
+++ src/main/java/io/github/lorimedeiros/cadastro_autores_api/controller/AutorController.java
@@ -8,8 +8,10 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/autores")
@@ -64,6 +66,25 @@ public class AutorController {
 
         service.deletar(autorOptional.get());
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping //obs: não podemos ter métodos repetidos, se eu passasse um get com parâmetro, igual o método 'obterDetalhes' ocorreria um conflito
+    public ResponseEntity<List<AutorDTO>> pesquisar(
+            @RequestParam(value = "nome", required = false) String nome,
+            @RequestParam(value = "nacionalidade", required = false) String nacionalidaade){ //esse required false significa que são opcionais, não obrigatórios
+
+        List<Autor> resultado = service.pesquisa(nome, nacionalidaade);
+        List<AutorDTO> lista = resultado
+                .stream()
+                .map(autor -> new AutorDTO(
+                        autor.getId(),
+                        autor.getNome(),
+                        autor.getDataNascimento(),
+                        autor.getNacionalidade())
+                ).collect(Collectors.toList());
+
+        return ResponseEntity.ok(lista);
+
     }
 
 }
--- src/main/java/io/github/lorimedeiros/cadastro_autores_api/repository/AutorRepository.java
+++ src/main/java/io/github/lorimedeiros/cadastro_autores_api/repository/AutorRepository.java
@@ -3,6 +3,13 @@ package io.github.lorimedeiros.cadastro_autores_api.repository;
 import io.github.lorimedeiros.cadastro_autores_api.model.Autor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
-public interface AutorRepository extends JpaRepository<Autor, UUID> { }
+public interface AutorRepository extends JpaRepository<Autor, UUID> {
+
+    List<Autor> findByNome(String nome);
+    List<Autor> findByNacionalidade (String nacionalidade);
+    List<Autor> findByNomeAndNacionalidade(String nome, String nacionalidade);
+
+}
