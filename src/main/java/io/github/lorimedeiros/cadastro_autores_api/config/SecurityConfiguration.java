cebcaa91c2b5b53baedb2834f1e09b3c1ac28310
Lori Medeiros <cecilialmedeirosc@gmail.com>
2025-05-05 19:49:16 -0300

    4.4 Algumas anotações sobre Http Basic e como funciona a base64

--- src/main/java/io/github/lorimedeiros/cadastro_autores_api/config/SecurityConfiguration.java
+++ src/main/java/io/github/lorimedeiros/cadastro_autores_api/config/SecurityConfiguration.java
@@ -12,25 +12,32 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfiguration {
 
-    //essa config é a exata configuração padrão de quando apenas adicionamos a spring security sem configurar nada,
-    //ou seja, isso é apenas uma demonstração.
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         return http
-                .csrf(AbstractHttpConfigurer::disable) //como não estamos trabalhando com aplicação web, isso está desabilitado
-                .formLogin(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
                 .httpBasic(Customizer.withDefaults())
                 .authorizeHttpRequests(authorize -> {
                     authorize.anyRequest().authenticated();
-                    //qualquer requisição que eu fizer tem que estar autenticada
                 })
                 .build();
     }
 
+    //httpBasic -> de aplicação para aplicação (por isso usamos no postman
+    //formLogin -> de usuário (por isso usamos no navegador
+    //OBS: se remover o formulário de login ainda é possivel se credenciar pela web, mas é usando Http Basic
+
     /*
-    .formLogin(configurer -> configurer.loginPage("/login.html").successForwardUrl("/home.html"))
-    se eu tivesse arquivos html assim eu conectaria essas paginas
+    Como funciona o sistema da base64 do Basic:
+    1 - No postman, vá em Authorization
+    2 - Selecione Auth Type 'No Auth'
+    3 - Vá em Headers
+    4 - Crie o header com key 'Authorization' e value 'Basic' + spacebar
+    5 - Vá em: https://www.debugbear.com/basic-auth-header-generator
+    6 - Coloque usuário (por padrão é 'user') e a senha dada pelo terminal spring
+    7 - Pegue o hash criado pelo site e passe no value da autenticação no postman (depois do spacebar)
+    8 - Feito!
+    É, basicamente, isso que o Basic faz só que de forma automática no postman quando colocamos user e senha
     */
 
 }
