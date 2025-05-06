9734e2871d33d485598f212d5110a635b844d115
Lori Medeiros <cecilialmedeirosc@gmail.com>
2025-05-06 20:33:45 -0300

    4.7 Criando usuários em memória (para demonstração)

--- src/main/java/io/github/lorimedeiros/cadastro_autores_api/Application.java
+++ src/main/java/io/github/lorimedeiros/cadastro_autores_api/Application.java
@@ -12,4 +12,9 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
+	// 1 -> criado método userDetailsService na classe SecurityConfiguration
+	// 2 -> criado o codificador e decodificador, que é o passwordEncoder usando BCrypt
+	// 3 -> foi colcado o encoder.encode nos passwords de userDetailsService
+	// FINAL -> depois disso é só dar uma brincadinha no Postman com Basic Auth ou usando o form np browser
+
 }
--- src/main/java/io/github/lorimedeiros/cadastro_autores_api/config/SecurityConfiguration.java
+++ src/main/java/io/github/lorimedeiros/cadastro_autores_api/config/SecurityConfiguration.java
@@ -6,6 +6,12 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -24,6 +30,35 @@ public class SecurityConfiguration {
                     authorize.anyRequest().authenticated();
                 })
                 .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder(10); //força: dizer quantas vezes vai passar em cima daquele B Crypt
+        //considerado bastante seguro
+        //funcionamento: uma vez que a senha é codificada, não é possível voltar atrás
+        //ou seja, se pegar o hash B Crypt, não é possível descobrir o que ele significa.
+        //MAS, ele consegue comparar o hash com a senha digitada e dizer se é compatível.
+        //ele diz se a string foi usada para gerar aquele hash
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder encoder){
+
+        UserDetails user1 = User.builder()
+                .username("usuario")
+                .password(encoder.encode("123"))
+                .roles("USER")
+                .build();
+
+        UserDetails user2 = User.builder()
+                .username("admin")
+                .password(encoder.encode("321"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user1, user2);
+
     }
 
 }
