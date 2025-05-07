be24d68458f5b351d700b9bcf19589ae57d551a7
Lori Medeiros <cecilialmedeirosc@gmail.com>
2025-05-07 20:19:01 -0300

    4.9 Criação da estrutura de usuários no banco de dados

--- src/main/java/io/github/lorimedeiros/cadastro_autores_api/Application.java
+++ src/main/java/io/github/lorimedeiros/cadastro_autores_api/Application.java
@@ -16,7 +16,7 @@ public class Application {
 	// 2 -> script da tabela usuario adicionado + criação da tabela no postgres
 	// 3 -> info na pasta external libraries do projeto, clicando em qualquer lugar e digitando 'hibernate' achamos o core
 	// 4 -> no core está a versão do hibernate, para que possamos adicionar a dependencia que nos ajudará a transformar as strings da nossa lista 'roles' em array
-	// VERSÃO: 6.6.11
+	// VERSÃO: 6.6
 	// OBS: o hibernate sozinho não consegue fazer isso, por isso vamos resolver com uma dependencia extra, a 'hypersistence'
 	// https://github.com/vladmihalcea/hypersistence-utils
 	// 5 -> buscando por versão, colocamos no pom.xml e carregamos o maven
