bc3efc91622b8c70a6fd0cf6ded54e2757d2b42e
lorimedeiros <lorimedeirosc@gmail.com>
2025-05-28 21:36:49 -0300

    9.0 Utilizando Actuator para obter métricas da aplicação

--- src/main/java/io/github/lorimedeiros/cadastro_autores_api/Application.java
+++ src/main/java/io/github/lorimedeiros/cadastro_autores_api/Application.java
@@ -13,12 +13,18 @@ public class Application {
 	}
 
 	/*
-	Customizando e adicionando cores nos logs:
-	1 -> No application.yml, no mesmo nível de application (só isso já habilita cores, mas iremos customizar)
-		   output:
-    	     ansi:
-      		   enabled: ALWAYS
-    2 -> em 'logging: pattern: console:' está a string de configuração de cores, formato da data foi mudado para o brasileiro
+	Utilizando Actuator para obter métricas da aplicação (observalidade)
+	  - Monitoramento é um ponto muito importante no pós deploy
+	    são usadas ferramentas como Prometheus e Grafana para a observalidade.
+	    O Actuator pode prover informações para essas ferramentas.
+
+	1 -> dependencia do actuator adicionada ao pom.xml
+	2 -> configurando para a url do actuator ser ignorada pelo spring security em securityConfiguration
+	3 -> acessando http://localhost:8080/actuator vemos a mensagem que indica que o actuator está ativado
+		 podemos ver também os endpoints habilitados
+	4 -> acessando http://localhost:8080/actuator/health vemos a saúde da aplicação
+	5 -> ativados mais endpoints em application.yml, acessando novamente o endpoint do actuator vemos os novos endpoints
+	6 -> acessando http://localhost:8080/actuator/metrics vemos as métricas
 	*/
 
 }
