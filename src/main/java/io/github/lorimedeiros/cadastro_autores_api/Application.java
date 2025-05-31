74bd473eb58ec90fc9557d0fc5328b702338349a
lorimedeiros <lorimedeirosc@gmail.com>
2025-05-30 21:18:33 -0300

    9.4 Preparando o banco de dados de produção no docker

--- src/main/java/io/github/lorimedeiros/cadastro_autores_api/Application.java
+++ src/main/java/io/github/lorimedeiros/cadastro_autores_api/Application.java
@@ -13,20 +13,43 @@ public class Application {
 	}
 
 	/*
-	Utilizando variáveis de ambiente no lugar de profiles
-	1 -> no yml, foi trocada a 'datasource:url:' pela variável de ambiente '${DATASOURCE_URL}'
-		 dentro das chaves, após os dois pontos (:) está o valor padrão, é como dizer:
-		 "se não encontrar tal variável de ambiente, use..."
-		 com as demais foi feito o mesmo
-	2 -> na classe 'DatabaseConfiguration' do pacote 'config', foi posto o log para que seja informada a url do banco
-
-	OBS: uma observação, se não quiser usar nenhum perfil específico de yml, colocar default, isso irá pegar o principal
-	OBS2: se der um 'ctrl + F' no log, é possível buscar pela str digitada
-	OBS3: para definir algum outro valor na variável de ambiente, que não seja aquele padrão após o ':'
-		  é só ir nas configurações de variável de ambiente da IDE e colocar name como sendo o que vem antes
-		  dos ':' e o value sendo o outro valor que queremos atribuir.
-		  Dessa forma, o valor padrão (que vem depois do :) é ignorado, dando prioridade ao atribuído nas
-		  configurações de variável de ambiente da IDE.
+	Preparando o banco de dados de produção no docker
+	1 -> abrimo o cmd
+	2 -> colamos:
+	 	 docker run --name livrariadb-prod -d -p 5433:5432 -e POSTGRES_PASSWORD=postgresprod -e POSTGRES_USER=postgresprod -e POSTGRES_DB=livraria --network livraria-network postgres:17.4
+		 verificamos se está tudo ok com
+		 docker ps
+	3 -> agora temos que registrar um novo servidor
+		 a) acessamos: http://localhost:15432
+		 b) logamos normal com as mesmas informações do db principal
+		 c) criamos novo servidor: servers (botão direito) > register > server...
+		 d) na nova janela que abrirá, na aba general, name: producao
+		 e) aba connection: Host name/address: livrariadb-prod (nome do container)
+		 					port: 5432 (estão na mesma network então podem estar na mesma port)
+		 					maintenace database: livraria (nome que definimos no --name)
+		 					username: postgresprod
+		 					password: postgresprod
+		 f) damos um save e já podemos ver 'producao' no nível abaixo de servers (na aba de navegação esquerda)
+		 g) não tem nenhuma table, então copiamos os comandos de 'comandos-tabelas.txt' e rodamos na query tool
+		 OBS: na parte '-p 5433:5432', a segunda parte (5432) nunca muda, pois é a porta do container
+	4 -> feito isso, vamos dar insert em um usuário (pelo próprio querry tool) para que seja possível acessar
+		 a) antes vamos em: https://bcrypt-generator.com/
+		 	para encriptar a senha, pois nossa aplicação só deve aceitar assim (usei senha: admin123)
+		 b) agora teremos que habilitar uma função no postgre com o comando:
+		 	CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+		 c) agora o insert:
+		 	insert into usuario
+			(id, login, senha, email, roles)
+			values
+			(uuid_generate_v4(), 'gerente', '$2a$12$TsSzIh4qwdIQkAssCRkHq.t/WK0.Lg1isMVgiTTPSvJY8cSb8re8.', 'gerente@email.com', '{GERENTE}');
+	5 -> agora daremos insert em um client
+		 a) secret também deve ser encriptado (usei secret: secret-production)
+		 B: INSERT de client:
+		 	insert into client
+			(id, client_id, client_secret, redirect_uri, scope)
+			values
+			(uuid_generate_v4(), 'client-production', '$2a$12$y6oaBL/iGR/it83UP6yxgujsH3O.V309P4XShtlUy91o1mXkPraD6', 'http://localhost:8080/authorized', 'GERENTE');
+	6 ->
 	*/
 
 }
