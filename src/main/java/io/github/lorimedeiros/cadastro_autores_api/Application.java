47699561090c2ffadf67bd3820b46caa026686a2
lorimedeiros <lorimedeirosc@gmail.com>
2025-06-03 20:41:46 -0300

    9.7 Construindo a imagem Docker da aplicação

--- src/main/java/io/github/lorimedeiros/cadastro_autores_api/Application.java
+++ src/main/java/io/github/lorimedeiros/cadastro_autores_api/Application.java
@@ -13,30 +13,54 @@ public class Application {
 	}
 
 	/*
-	Criando um JAR da aplicação e rodando para entender o fluxo de build
+	Construindo a imagem Docker da aplicação através do Dockerfile
+	1 -> Na pasta raíz do projeto (cadastro-autores-api), click direito > new > file
+		 e criamos a file com nome 'Dockerfile'
 
-	DE FORMA AUTOMÁTICA:
-	1 -> vamos na aba Maven da IDE e selecionamos clean e package no pacote lifecycle
+	OBS: caso ainda não tenha o plugin do docker na IDE, intalar
 
-	USANDO LINHA DE COMANDO (NA MÃO):
-	1 -> abre o terminal da IDE e damos o comando " ./mvnw clean package -DskipTests "
-		 a) ./mvnw clean package : empacota a aplicação pra produção (builda e empacota gerando arquivo .jar)
-		 b) -DskipTests          : equivalente a desabilitar os testes na aba maven
+	2 -> Na IDE podemos ver a aba docker. Na barra inferior, no mesmo nível que terminal temos 'Services'
+		 click nele e deverá aparecer Docker com seus container e afins
 
-	2 -> com isso já podemos ver e acessar o arquivo jar. Na pasta raiz do projeto, no mesmo nível que src
-		 temos a pasta target, dentro dela temos nosso .jar (não o .original)
+	3 -> Agora teremos que construir esse arquivo docker com base em 2 partes 'build' e 'run'
+		 1º O Build:
+		 	CONTEXTO:  para fazer o build precisamos ir no maven e dar um clean + package
+		 		       ou seja, para fazer um build precisaremos da imagem do maven
+		 	RESOLUÇÃO: vamos no navegador e buscamos por https://hub.docker.com/ onde estão registradas
+		 			   as imagens docker da comunidade.
+		 			   Na barra de search buscamos por 'maven' e selecionamos o oficial (geralmente o primeiro)
+		 			   Click em 'tags'
 
-	3 -> CONCERTADO O NOME DA DATABASE NO .YML
+		 			   (depois vamos no filter tags e digitamos 21 (que é a versão da jdk que estou
+		 			   usando, mas isso pode ser ajustado de projeto pra projeto)
+		 			   Pode ser usado qualquer uma daquelas imagens, portanto que a versão bata)
 
-	DE FORMA AUTOMÁTICA:
-	4 -> click com botão direito no .jar e damos um run
+		 			   Buscamos pela tag '3.8.8-amazoncorretto-21-al2023'
+		 			   e colamos no dockerfile
 
-	USANDO LINHA DE COMANDO (NA MÃO):
-	4 -> no terminal: java -jar .\target\cadastro-autores-api-0.0.1-SNAPSHOT.jar
-	     a) selecionando a pasta (com click) na aba de navegação de pastas (na esquerda da IDE),
-	     	digitando até '.\target\' e dando um tab, acontece o auto-complete
+		   EXPLICAÇÃO: FROM maven:3.8.8-amazoncorretto-21-al2023
+		 			   FROM  : comando docker
+		 			   maven : nome da imagem
+		 			   o que vem depois dos ':' é a tag
 
-	OBS: caso haja falha em rodar o .jar, tentar novamente o mesmo comando no terminal
+					   WORKDIR /build
+					   construindo dentro da imagem uma pasta build
+					   pode ser dado o nome que quiser
+
+					   COPY . .
+					   comando para copiar que resebe 2 parâmetros
+					   primeiro . é qual arquivo quer copiar (toda pasta atual, todo o projeto
+					   segundo . para botar dentro da pasta build, issó funciona pois o comando acima já faz com que build seja a pasta selecionada para execução de comandos
+
+					   RUN mvn clean package -DskipTests
+					   mesmo comando usado anteriormente para criar o jar
+
+		 2º O RUN: aqui pegamos o jar criado dentro do build e copiamos dentro da pasta app
+		 		   exportmaos também as portas 8080, da aplicação e a 9090, do actuator com EXPOSE
+		 		   definimos quais variáveis de ambiente usaremos com ENV, as definimos como vazias, por hora
+		 		   já em spring profiles definimos o perfil como production
+				   definimos timezone com TZ
+				   ENTRYPOINT é o comando para inicializar a aplicação
 
 	*/
 
