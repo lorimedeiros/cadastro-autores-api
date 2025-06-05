7f0b45131e4221b53e26403e261ef567b43ec3a4
lorimedeiros <lorimedeirosc@gmail.com>
2025-06-04 22:51:23 -0300

    X.2 Criando a instancia do banco de dados no serviço RDS da AWS

--- src/main/java/io/github/lorimedeiros/cadastro_autores_api/Application.java
+++ src/main/java/io/github/lorimedeiros/cadastro_autores_api/Application.java
@@ -13,32 +13,27 @@ public class Application {
 	}
 
 	/*
-	Introdução aos serviços de Cloud
+	Criando a instancia do banco de dados no serviço RDS da AWS
+	1 -> acessamos o console de serviços aws (fazer login): https://sa-east-1.console.aws.amazon.com/console/home?region=sa-east-1#
+		 obs: na hora de realizar login há também uma tela de login como IAM, que são aqueles que acessam a
+		 	  aplicação de outra pessoa. Lembrar de logar como host.
+	2  -> click no icone da 'Aurora and RDS' (ou pesquisamos pelo serviço)
+	3  -> scrollamos até achar a opção 'Criar banco de dados'
+	4  -> em 'Escolher um método de criação de banco de dados' escolhemos 'criação padrão'
+	5  -> em 'Opções do mecanismo' escolhemos o 'PostgreSQL' que é o banco que estamos usando na aplicação
+	6  -> não habilitamos o filtro nem o RDS Extended Support
+	7  -> modelo definimos como o gratuito
+	8  -> configurações: nome = 'livrariaapi-database' (mas é o nome que quiser)
+	9  -> Configurações de credenciais: user = 'postgres' , senha = 'postgres'
+	10 -> Acesso público: sim (iremos acessar por fora da AWS)
+	11 -> desabilitamos o insights de monitoramento (é pago)
+	12 -> o resto deicamos no padrão
+	13 -> por fim criamos o banco de dados
+	14 -> click no nome do banco de dados para ver detalhes
 
-	- São serviços que são fornecidos (podendo ser gratuitos, testes ou pagos),
-	  esses serviços são, por exemplo: bancos de dados, repositórios git, registry docker, cloud computer.
-	  Esses serviços são caracterizados pela necessidade de internet para serem acessados.
-
-	- IAM: Identity Acess Management:
-	  Todos os serviços tem um controle de acesso total, onde se pode criar uma conta e é
-	  possível adicionar outros usuários para acessar as aplicações/serviços, além de
-	  controlar o nível de acesso das pessoas, tudo isso através desses serviços
-
-	- Service Acount:
-	  contas especificas para aplicações, afim de não expor suas credenciais
-
-	- Exemplos de serviços cloud: GCP (Google Cloud Plataform), AWS (Amazon Web Services)...
-
-	- Acessando... (GCP):
-	  No site da google cloud você realiza login com sua conta google e procura pelo console,
-	  que é o painel administrativo dos serviços.
-	  Na aba de navegação, acessada pelos três pontos, podemos ver todos os serviços.
-
-	- Acessando... (AWS):
-	  Bem semelhante, abrimos a pagina da AWS, logamos e buscamos pela aba de serviços ou console.
-	  * Alguns serviços:
-	  	+ EC2: Serviço pra Cloud Compute, para criar máquinas virtuais. (criarei uma para fazer deploy do container docker)
-	  	+ RDS: Serviço de Banco de Dados
+	Caso deslogue e queira acessar novemente:
+	1  -> acessamos a aba do serviço RDS
+	2  -> clicamos em 'instancias de banco de dados'
 	*/
 
 }
